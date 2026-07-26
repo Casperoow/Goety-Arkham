01a73458a6fcb47f50034a03662fa772e5912d4e
Casper <casper.nong@outlook.com>
2026-07-26 11:29:04 +1000

    Initialize Goety Arkham addon project

---
+++ src/main/java/com/casper/goetyarkham/GoetyArkham.java
@@ -0,0 +1,11 @@
+package com.casper.goetyarkham;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GoetyArkham.MOD_ID)
+public final class GoetyArkham {
+    public static final String MOD_ID = "goetyarkham";
+
+    public GoetyArkham() {
+    }
+}
